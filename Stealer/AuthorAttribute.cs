9fe47a8f72332431e82a85a240f8d452a22bbb73
Svetlin Stoyanov <svetlinst@gmail.com>
2019-11-02 19:15:37 +0200

    solutions for problems 5 and 6

---
+++ Stealer/AuthorAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Stealer
+{
+    using System;
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorAttribute:Attribute
+    {
+        public string Name { get; set; }
+
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
--- Stealer/StartUp.cs
+++ Stealer/StartUp.cs
@@ -4,11 +4,14 @@
 
     public class StartUp
     {
+        [Author("SvetlinStoyanov")]
         public static void Main(string[] args)
         {
             Spy spy = new Spy();
             string result = spy.CollectGettersAndSetters("Hacker");
             Console.WriteLine(result);
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
             
         }
     }
